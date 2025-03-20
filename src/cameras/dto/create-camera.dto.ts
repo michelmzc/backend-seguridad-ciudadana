708cb70646c3077e0c4f2c1c1f2dcf960a9a8f8d
michelmzc <michelmzco@gmail.com>
2025-03-20 12:10:10 -0300

    agregando dto de camaras

--- src/cameras/dto/create-camera.dto.ts
+++ src/cameras/dto/create-camera.dto.ts
@@ -1,1 +1,18 @@
-export class CreateCameraDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+
+export class CreateCameraDto {
+    // decorador para definir una propiedad para la documentación en Swagger OpenAPI
+    @ApiProperty({
+        example: "IR Eyeball Camera"
+    })
+    readonly name: string; //definición de un campo
+
+    @ApiProperty({ example: "rtsp://example.com"})
+    readonly stream_url: string;
+
+    @ApiProperty({
+        example: ['NestJS', 'REST API']
+    })
+    readonly keywords: string []; // ejemplo con un array
+}
