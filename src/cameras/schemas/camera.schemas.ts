2ffb6befbae64f0fbbf8af5ca9f90c046b1cf161
michelmzc <michelmzco@gmail.com>
2025-03-20 10:54:28 -0300

    agregando esquema de cámaras

--- src/cameras/schemas/camera.schemas.ts
+++ src/cameras/schemas/camera.schemas.ts
@@ -1,1 +1,23 @@
-export class Camera {}
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose"
+import  { Document } from "mongoose";
+
+// definición del tipo de un documento libro
+export type CameraDocument = Camera & Document;
+
+@Schema() // decorador para crear una colección MongoDB para la clase
+export class Camera {
+    @Prop() // decorador para añadir un campo a la colección
+    name: string;
+
+    @Prop()
+    stream_url: string;
+
+    @Prop()
+    serial_number: string;
+
+    @Prop([String]) // indicación de tipo no primitivos para prueba
+    keywords: string[];
+}
+
+// esquema Mongoose creado a partir de la clase Camera
+export const BookSchema = SchemaFactory.createForClass(Camera);
