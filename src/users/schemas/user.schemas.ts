089965cf474429216d10563e8bda7016b87b2661
michelmzc <michelmzco@gmail.com>
2025-03-24 16:16:06 -0300

    agregando esquema y registrando usuario en módulo

--- src/users/schemas/user.schemas.ts
+++ src/users/schemas/user.schemas.ts
@@ -1,1 +1,23 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose'; // importamos desde mongoose
+
+// definición del tipo de documento usuario
+export type UserDocument = User & Document;
+
+
+@Schema()
+export class User {
+    @Prop()
+    _id: string; // gestión del id desde el código, saltamos mongo
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    email: string;
+
+    @Prop()
+    country: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
