06f25a27f8abb3be9eee4d02e09b2360cfdab683
michelmzc <michelmzco@gmail.com>
2025-05-29 12:34:07 -0400

    agregando firebase

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,7 +16,7 @@ export class UsersController {
   async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
-
+ 
   @Get()
   async findAll(@Query() query: any) {
     return await this.usersService.findAll(query);
