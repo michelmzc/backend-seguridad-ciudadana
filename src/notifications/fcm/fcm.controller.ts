1f5b942f5861943ec1eb28539829162b4aa246ef
michelmzc <michelmzco@gmail.com>
2025-06-05 17:49:04 -0400

    test menores de notificaciones

--- src/notifications/fcm/fcm.controller.ts
+++ src/notifications/fcm/fcm.controller.ts
@@ -1,20 +1,19 @@
 import { Controller, Post, Req, Body, Delete } from '@nestjs/common';
 import { FcmService } from './fcm.service';
-import { error } from 'console';
 
 @Controller('fcm')
 export class FcmController {
   constructor(private readonly fcmService: FcmService) {}
 
   @Post('register')
-  async register(@Req() req:Request,@Body() body: any) {
+  async register(@Req() req: Request, @Body() body: any) {
     console.log('FCM Register recibido:', body);
 
     console.log('>> req.body:', req.body)
     console.log('>> Headers:', req.headers)
 
     if (!body || !body.userId || !body.token ){
-      throw new error('Datos FCM incompletos');
+      throw new Error('Datos FCM incompletos');
     }
 
     return this.fcmService.registerToken(body.userId, body.token, body.platform);
