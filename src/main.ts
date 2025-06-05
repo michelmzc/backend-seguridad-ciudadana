f1a643f7361c0b5ac2b5fb627187e64a856597be
michelmzc <michelmzco@gmail.com>
2025-06-05 00:05:38 -0400

    aceptar peticiones de IP pública

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization'
   });
   
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 bootstrap();
