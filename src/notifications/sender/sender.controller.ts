9e5c93684d5a2cb8fb509fa9659d96d073e5e114
michelmzc <michelmzc@gmail.com>
2025-06-09 15:29:43 -0400

    agregando descripción de API para notificaciones

--- src/notifications/sender/sender.controller.ts
+++ src/notifications/sender/sender.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
 import { SenderService } from './sender.service';
 
 @Controller('notifications')
+@ApiTags('Notificaciones')
 export class SenderController {
   constructor(private readonly senderService: SenderService) {}
 
   @Post('send-test')
+  @ApiOperation({ summary: 'Test de notificaciones (necesita token de usuario)'})
   async sendTestNotification(@Body() body: { token: string }) {
     return this.senderService.sendNotificationToToken(
       body.token,
